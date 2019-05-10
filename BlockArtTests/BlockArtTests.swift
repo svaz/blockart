8bc121120d77b5d05c7f9e8286f1df52f09dd71b
Savio Vaz <svaz2000@hotmail.com>
2019-05-10 11:16:20 -0400

    Initial Commit

---
+++ BlockArtTests/BlockArtTests.swift
@@ -0,0 +1,34 @@
+//
+//  BlockArtTests.swift
+//  BlockArtTests
+//
+//  Created by savio vaz on 5/10/19.
+//  Copyright © 2019 savio vaz. All rights reserved.
+//
+
+import XCTest
+@testable import BlockArt
+
+class BlockArtTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
